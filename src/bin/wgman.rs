ec0398d4cc64deba931a96ae6fb9aa9c7287cb7b
Ashley Wulber <ashtronaut@pm.me>
2021-01-20 23:10:53 -0500

    feat: add routing

--- src/bin/wgman.rs
+++ src/bin/wgman.rs
@@ -1,16 +1,11 @@
-use uuid::Uuid;
-use warp::{Filter, Rejection, Reply};
+use warp::{Filter};
 
-type Result<T> = std::result::Result<T, Rejection>;
-
-use sqlx::postgres::PgPoolOptions;
-
-use wgman::config;
+use wgman::{config, handlers::*, filters};
+use wgman::dao::{connect};
 
 #[tokio::main]
 async fn main() {
     //db setup
-    // let config = Config::from_str("postgres://wireguarddb:postgres:kQHJZVrNz$VO@172.17.0.2:5432").unwrap();
     let db_cfg = match config::get_db_cfg() {
         Ok(cfg) => {cfg}
         Err(err) => {
@@ -18,109 +13,21 @@ async fn main() {
             std::process::exit(1);
         }
     };
-    dbg!(&db_cfg);
 
-    let pool = match PgPoolOptions::new()
-        .max_connections(20)
-        .connect(&format!("postgres://{}:{}@{}:{}/{}", db_cfg.user, db_cfg.pw, db_cfg.host, db_cfg.port, db_cfg.name)).await {
-            Ok(pool) => {pool}
-            Err(err) => {
-                dbg!(err);
-                std::process::exit(1);
-            }
-        };
-    dbg!(&pool);
-
-    let row = match sqlx::query("SELECT * FROM public.\"User\"").fetch_all(&pool).await {
-        Ok(row) => {
-            row
-        },
-        Err(err) => {
-            dbg!(err);
+    let pool = match connect(db_cfg).await {
+        Ok(u) => {u},
+        Err(_) => {
             std::process::exit(1);
         }
     };
 
-    println!("DB connection successful");
-
-
-    // warp setup
-    let set = warp::path!("set" / String).and(warp::post());
-    let remove = warp::path!("remove" / String).and(warp::post());
-    let list = warp::path!("list" / String).and(warp::post());
-
-    let interface = warp::path("interface");
-    let peer = warp::path("peer");
-    let endpoint = warp::path("endpoint");
-
-    let interface_set = interface.and(set).and_then(interface_set_handler);
-    let interface_remove = interface.and(remove).and_then(interface_remove_handler);
-    let interface_list = interface.and(list).and_then(interface_list_handler);
-    
-    let peer_set = peer.and(set).and_then(peer_set_handler);
-    let peer_remove = peer.and(remove).and_then(peer_remove_handler);
-    let peer_list = peer.and(list).and_then(peer_list_handler);
-    
-    let endpoint_set = endpoint.and(set).and_then(endpoint_set_handler);
-    let endpoint_remove = endpoint.and(remove).and_then(endpoint_remove_handler);
-    let endpoint_list = endpoint.and(list).and_then(endpoint_list_handler);
-
-    let api_routes = (interface_set
-            .or(interface_remove)
-            .or(interface_list)
-            .or(peer_set)
-            .or(peer_remove)
-            .or(peer_list)
-            .or(endpoint_set)
-            .or(endpoint_remove)
-            .or(endpoint_list)).with(warp::cors().allow_any_origin());
-
-    let health_route = warp::path("health").and(warp::get()
-        .and_then(health_handler)
-        .with(warp::cors().allow_any_origin())
-        );
-    let routes = health_route.or(api_routes);
+    // routes
+    let health_route = warp::path("health")
+        .and(warp::get())
+        .and_then(health);
+    let routes = health_route.or(filters::auth(pool));
 
     println!("Started server at localhost:8000");
     warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
 
-async fn health_handler() -> Result<impl Reply> {
-    Ok("OK")
-}
-
-async fn interface_set_handler(_auth_type: String) -> Result<impl Reply> {
-    Ok("interface_set_handler")
-}
-
-async fn interface_remove_handler(_auth_type: String) -> Result<impl Reply> {
-    Ok("interface_remove_handler")
-}
-
-async fn interface_list_handler(_auth_type: String) -> Result<impl Reply> {
-    Ok("interface_list_handler")
-}
-
-async fn peer_set_handler(_auth_type: String) -> Result<impl Reply> {
-    Ok("peer_set_handler")
-}
-
-async fn peer_remove_handler(_auth_type: String) -> Result<impl Reply> {
-    Ok("peer_remove_handler")
-}
-
-async fn peer_list_handler(_auth_type: String) -> Result<impl Reply> {
-    Ok("peer_list_handler")
-}
-
-async fn endpoint_set_handler(_auth_type: String) -> Result<impl Reply> {
-    Ok("endpoint_set_handler")
-}
-
-async fn endpoint_remove_handler(_auth_type: String) -> Result<impl Reply> {
-    Ok("endpoint_remove_handler")
-}
-
-async fn endpoint_list_handler(_auth_type: String) -> Result<impl Reply> {
-    Ok("endpoint_list_handler")
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,86 @@
 pub mod auth {
-    use data_encoding::HEXUPPER;
+    use base64::decode;
     use ring::error::Unspecified;
     use ring::rand::SecureRandom;
     use ring::{digest, pbkdf2, rand};
-    use std::num::NonZeroU32;
-    struct Hash {
-        pbkdf2_hash: [u8; digest::SHA512_OUTPUT_LEN],
-        salt: [u8; digest::SHA512_OUTPUT_LEN],
+    use sqlx::{Pool, Postgres};
+    use std::{convert::TryInto, error::Error, num::NonZeroU32, str::FromStr};
+
+    use crate::{dao::{InterfacePassword, UserPassword, get_interface_pw, get_user_pw}};
+
+    pub struct Hash {
+        pub pbkdf2_hash: [u8; digest::SHA512_OUTPUT_LEN],
+        pub salt: [u8; digest::SHA512_OUTPUT_LEN],
     }
     
-    fn encrypt(password: &str) -> Result<Hash, Unspecified> {
+    #[derive(Debug, Default, Clone)]
+    pub struct BasicAuth {
+        name: String,
+        password: String
+    }
+
+    #[derive(Debug, Clone)]
+    pub enum AuthKind {
+        User,
+        Interface,
+    }
+
+    impl Default for AuthKind {
+        fn default() -> Self {
+            AuthKind::User
+        }
+    }
+
+    // Warning:: only intended to be used with base64 authorization header
+    impl FromStr for BasicAuth {
+        type Err = Box<dyn Error>;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            match &s[..5] {
+                "Basic" => {
+                    let decoded = decode(&s[6..])?;
+                    let auth_string = std::str::from_utf8(&decoded[..])?;
+                    let colon_indx = match auth_string.find(":") {
+                        Some(indx) => {
+                            if indx < auth_string.len() - 1 {
+                                indx
+                            }
+                            else {
+                                Err("Invalid Login")?
+                            }
+                        },
+                        None => {Err("Invalid Login")?}
+                    };
+
+                    Ok(BasicAuth { name: auth_string[..colon_indx].into(), password: auth_string[colon_indx + 1..].into() })
+                        
+                }
+                _ => Err("Invalid Login")?
+            }
+        }
+    }
+
+    #[tokio::main]
+    async fn authenticate(auth_type: AuthKind, BasicAuth { name, password } : &BasicAuth, pool: &Pool<Postgres>) -> Result<(), Box<dyn Error>> {
+        match auth_type {
+            AuthKind::User => {
+                let UserPassword { id: _, password_hash, salt }: UserPassword = get_user_pw(pool, name.clone()).await?;
+                match verify(&Hash { pbkdf2_hash: password_hash.as_bytes().try_into()?, salt: salt.as_bytes().try_into()? }, password) {
+                    Ok(_) => {Ok(())}
+                    Err(_) => {Err("invalid login")?}
+                }
+            }
+            AuthKind::Interface => {
+                let InterfacePassword { id: _, password_hash, salt }: InterfacePassword = get_interface_pw(pool, name.clone()).await?;
+                match verify(&Hash { pbkdf2_hash: password_hash.as_bytes().try_into()?, salt: salt.as_bytes().try_into()? }, password) {
+                    Ok(_) => {Ok(())}
+                    Err(_) => {Err("invalid login")?}
+                }
+            }
+        }
+    }
+
+    pub fn encrypt(password: &str) -> Result<Hash, Unspecified> {
         const CREDENTIAL_LEN: usize = digest::SHA512_OUTPUT_LEN;
         let n_iter = NonZeroU32::new(100_000).unwrap();
         let rng = rand::SystemRandom::new();
@@ -29,7 +100,7 @@ pub mod auth {
         Ok(Hash { salt, pbkdf2_hash })
     }
     
-    fn verify(Hash { salt, pbkdf2_hash }: &Hash, password: &str) -> Result<(), Unspecified> {
+    pub(crate) fn verify(Hash { salt, pbkdf2_hash }: &Hash, password: &str) -> Result<(), Unspecified> {
         let n_iter = NonZeroU32::new(100_000).unwrap();
         const CREDENTIAL_LEN: usize = digest::SHA512_OUTPUT_LEN;
     
@@ -41,6 +112,8 @@ pub mod auth {
             pbkdf2_hash,
         )
     }
+
+
 }
 
 pub mod config {
@@ -80,5 +153,292 @@ pub mod config {
 }
 
 pub mod dao {
+    use ipnetwork::IpNetwork;
+    use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
+    use uuid::Uuid;
+    use crate::config::DbCfg;
 
+    #[derive(Debug, Clone, sqlx::FromRow)]
+    pub struct User {
+        pub id: Uuid,
+        pub name: String,
+        pub is_admin: bool,
+    }
+
+    #[derive(Debug, Clone, sqlx::FromRow)]
+    pub struct UserPassword {
+        pub id: Uuid,
+        pub password_hash: String,
+        pub salt: String,
+    }
+
+    #[derive(Debug, Clone, sqlx::FromRow)]
+    pub struct Interface {
+        pub id: Uuid,
+        pub name: String,
+        pub public_key: Option<String>,
+        pub port: Option<i32>,
+        pub ip: Option<IpNetwork>,
+        pub fqdn: Option<String>,
+    }
+
+    #[derive(Debug, Clone, sqlx::FromRow)]
+    pub struct InterfacePassword {
+        pub id: Uuid,
+        pub password_hash: String,
+        pub salt: String,
+    }
+
+    #[derive(Debug, Clone, sqlx::FromRow)]
+    pub struct PeerRelation {
+        pub endpoint: Uuid,
+        pub peer: Uuid,
+        endpoint_allowed_ip: Vec<IpNetwork>,
+        peer_allowed_ip: Vec<IpNetwork>,
+    }
+
+    pub async fn connect(db_cfg: DbCfg) -> Result<Pool<Postgres>, sqlx::Error> {
+        Ok(PgPoolOptions::new()
+            .max_connections(20)
+            .connect(&format!("postgres://{}:{}@{}:{}/{}", db_cfg.user, db_cfg.pw, db_cfg.host, db_cfg.port, db_cfg.name))
+            .await?
+        )
+    }
+
+    pub async fn get_user_by_name(pool: &Pool<Postgres>, name: String) -> Result<User, sqlx::Error> {
+        Ok(sqlx::query_as!(User, "SELECT * FROM public.\"User\" Where name = $1", name).fetch_one(pool).await?)
+    }
+
+    pub async fn get_interface_by_name(pool: &Pool<Postgres>, name: String) -> Result<Interface, sqlx::Error> {
+        Ok(sqlx::query_as!(Interface, "SELECT * FROM public.\"Interface\" Where name = $1", name).fetch_one(pool).await?)
+    }
+
+    pub async fn get_interfaces(pool: &Pool<Postgres>) -> Result<Vec<Interface>, sqlx::Error> {
+        Ok(sqlx::query_as!(Interface, "SELECT * FROM public.\"Interface\"").fetch_all(pool).await?)
+    }
+
+    pub async fn get_user_pw(pool: &Pool<Postgres>, name: String) -> Result<UserPassword, sqlx::Error> {
+        Ok(sqlx::query_as!(UserPassword, "SELECT pw.id, password_hash, salt FROM public.\"User\" u INNER JOIN public.\"UserPassword\" pw ON u.id= pw.id WHERE name = $1", name).fetch_one(pool).await?)
+    }
+
+    pub async fn get_interface_pw(pool: &Pool<Postgres>, name: String) -> Result<InterfacePassword, sqlx::Error> {
+        Ok(sqlx::query_as!(InterfacePassword, "SELECT pw.id, password_hash, salt FROM public.\"User\" i INNER JOIN public.\"InterfacePassword\" pw ON i.id= pw.id WHERE name = $1", name).fetch_one(pool).await?)
+    }
+
+    pub async fn get_peers(pool: &Pool<Postgres>, id: Uuid) -> Result<Vec<PeerRelation>, sqlx::Error> {
+        Ok(sqlx::query_as!(PeerRelation, "SELECT * FROM public.\"PeerRelation\" Where endpoint = $1::UUID", id).fetch_all(pool).await?)
+    }
+
+    pub async fn get_(pool: &Pool<Postgres>, id: Uuid) -> Result<Vec<PeerRelation>, sqlx::Error> {
+        Ok(sqlx::query_as!(PeerRelation, "SELECT * FROM public.\"PeerRelation\" Where peer = $1::UUID", id).fetch_all(pool).await?)
+    }
+}
+
+pub mod handlers {
+    use std::convert::Infallible;
+    use sqlx::{Pool, Postgres};
+    use warp::{Reply, Rejection};
+    use crate::auth::{AuthKind, BasicAuth};
+    
+    type WarpResult<T> = std::result::Result<T, Rejection>;
+
+    #[derive(Debug, Default, Clone)]
+    pub struct LoginBody {
+        auth_type: AuthKind,
+        name: String,
+        password: String
+    }
+
+
+    pub async fn health() -> Result<impl Reply, Infallible> {
+        Ok("OK")
+    }
+    
+    pub async fn interface_set(auth_kind: AuthKind, bauth: BasicAuth, db_pool: Pool<Postgres>) -> WarpResult<impl Reply> {
+        Ok("interface_set")
+    }
+    
+    pub async fn interface_remove(auth_kind: AuthKind, bauth: BasicAuth, db_pool: Pool<Postgres>) -> WarpResult<impl Reply> {
+        Ok("interface_remove")
+    }
+    
+    pub async fn interface_list(auth_kind: AuthKind, bauth: BasicAuth, db_pool: Pool<Postgres>) -> WarpResult<impl Reply> {
+        Ok("interface_list")
+    }
+    
+    pub async fn peer_set(auth_kind: AuthKind, bauth: BasicAuth, db_pool: Pool<Postgres>) -> WarpResult<impl Reply> {
+        Ok("peer_set")
+    }
+    
+    pub async fn peer_remove(auth_kind: AuthKind, bauth: BasicAuth, db_pool: Pool<Postgres>) -> WarpResult<impl Reply> {
+        Ok("peer_remove")
+    }
+    
+    pub async fn peer_list(auth_kind: AuthKind, bauth: BasicAuth, db_pool: Pool<Postgres>) -> WarpResult<impl Reply> {
+        Ok("peer_list")
+    }
+    
+    pub async fn endpoint_set(auth_kind: AuthKind, bauth: BasicAuth, db_pool: Pool<Postgres>) -> WarpResult<impl Reply> {
+        Ok("endpoint_set")
+    }
+    
+    pub async fn endpoint_remove(auth_kind: AuthKind, bauth: BasicAuth, db_pool: Pool<Postgres>) -> WarpResult<impl Reply> {
+        Ok("endpoint_remove")
+    }
+    
+    pub async fn endpoint_list(auth_kind: AuthKind, bauth: BasicAuth, db_pool: Pool<Postgres>) -> WarpResult<impl Reply> {
+        Ok("endpoint_list")
+    }
+}
+
+pub mod filters {
+    use sqlx::{Pool, Postgres};
+    use warp::Filter;
+
+    use crate::handlers;
+    use crate::auth::{AuthKind, BasicAuth};
+        
+
+    pub fn with_auth(auth_kind: AuthKind) -> impl Filter<Extract = (AuthKind,), Error = std::convert::Infallible> + Clone {
+        warp::any().map(move || auth_kind.clone())
+    }
+
+    pub fn with_db(db: Pool<Postgres>) -> impl Filter<Extract = (Pool<Postgres>,), Error = std::convert::Infallible> + Clone {
+        warp::any().map(move || db.clone())
+    }
+
+    pub fn auth(
+        pool: Pool<Postgres>
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::any()
+        // interface
+        .and(warp::path("iauth").and(interface_set(AuthKind::Interface, pool.clone()))
+            .or(warp::path("uauth").and(interface_set(AuthKind::User, pool.clone()))))
+        .or(warp::path("iauth").and(interface_list(AuthKind::Interface, pool.clone()))
+            .or(warp::path("uauth").and(interface_list(AuthKind::User, pool.clone()))))
+        .or(warp::path("iauth").and(interface_remove(AuthKind::Interface, pool.clone()))
+            .or(warp::path("uauth").and(interface_remove(AuthKind::User, pool.clone()))))
+        // peers
+        .or(warp::path("iauth").and(peer_set(AuthKind::Interface, pool.clone()))
+            .or(warp::path("uauth").and(peer_set(AuthKind::User, pool.clone()))))
+        .or(warp::path("iauth").and(peer_list(AuthKind::Interface, pool.clone()))
+            .or(warp::path("uauth").and(peer_list(AuthKind::User, pool.clone()))))
+        .or(warp::path("iauth").and(peer_remove(AuthKind::Interface, pool.clone()))
+            .or(warp::path("uauth").and(peer_remove(AuthKind::User, pool.clone()))))
+        // endpoints
+        .or(warp::path("iauth").and(endpoint_set(AuthKind::Interface, pool.clone()))
+            .or(warp::path("uauth").and(endpoint_set(AuthKind::User, pool.clone()))))
+        .or(warp::path("iauth").and(endpoint_list(AuthKind::Interface, pool.clone()))
+            .or(warp::path("uauth").and(endpoint_list(AuthKind::User, pool.clone()))))
+        .or(warp::path("iauth").and(endpoint_remove(AuthKind::Interface, pool.clone()))
+            .or(warp::path("uauth").and(endpoint_remove(AuthKind::User, pool.clone()))))
+    }
+
+    fn interface_set(
+        auth_kind: AuthKind, pool: Pool<Postgres>
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::post()
+        .and(warp::path("interface"))
+        .and(warp::path("set"))
+        .and(with_auth(auth_kind))
+        .and(warp::header::<BasicAuth>("authorization"))
+        .and(with_db(pool))
+        .and_then(handlers::interface_set)
+    }
+
+    fn interface_list(
+        auth_kind: AuthKind, pool: Pool<Postgres>
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::post()
+        .and(warp::path("interface"))
+        .and(warp::path("list"))
+        .and(with_auth(auth_kind))
+        .and(warp::header::<BasicAuth>("authorization"))
+        .and(with_db(pool))
+        .and_then(handlers::interface_list)
+    }
+
+    fn interface_remove(
+        auth_kind: AuthKind, pool: Pool<Postgres>
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::post()
+        .and(warp::path("interface"))
+        .and(warp::path("remove"))
+        .and(with_auth(auth_kind))
+        .and(warp::header::<BasicAuth>("authorization"))
+        .and(with_db(pool))
+        .and_then(handlers::interface_remove)
+    }
+
+    fn peer_set(
+        auth_kind: AuthKind, pool: Pool<Postgres>
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::post()
+        .and(warp::path("peer"))
+        .and(warp::path("set"))
+        .and(with_auth(auth_kind))
+        .and(warp::header::<BasicAuth>("authorization"))
+        .and(with_db(pool))
+        .and_then(handlers::peer_set)
+    }
+
+    fn peer_list(
+        auth_kind: AuthKind, pool: Pool<Postgres>
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::post()
+        .and(warp::path("peer"))
+        .and(warp::path("list"))
+        .and(with_auth(auth_kind))
+        .and(warp::header::<BasicAuth>("authorization"))
+        .and(with_db(pool))
+        .and_then(handlers::peer_list)
+    }
+
+    fn peer_remove(
+        auth_kind: AuthKind, pool: Pool<Postgres>
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::post()
+        .and(warp::path("peer"))
+        .and(warp::path("remove"))
+        .and(with_auth(auth_kind))
+        .and(warp::header::<BasicAuth>("authorization"))
+        .and(with_db(pool))
+        .and_then(handlers::peer_remove)
+    }
+
+    fn endpoint_set(
+        auth_kind: AuthKind, pool: Pool<Postgres>
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::post()
+        .and(warp::path("endpoint"))
+        .and(warp::path("set"))
+        .and(with_auth(auth_kind))
+        .and(warp::header::<BasicAuth>("authorization"))
+        .and(with_db(pool))
+        .and_then(handlers::endpoint_set)
+    }
+
+    fn endpoint_list(
+        auth_kind: AuthKind, pool: Pool<Postgres>
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::post()
+        .and(warp::path("endpoint"))
+        .and(warp::path("list"))
+        .and(with_auth(auth_kind))
+        .and(warp::header::<BasicAuth>("authorization"))
+        .and(with_db(pool))
+        .and_then(handlers::endpoint_list)
+    }
+
+    fn endpoint_remove(
+        auth_kind: AuthKind, pool: Pool<Postgres>
+    ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::post()
+        .and(warp::path("endpoint"))
+        .and(warp::path("remove"))
+        .and(with_auth(auth_kind))
+        .and(warp::header::<BasicAuth>("authorization"))
+        .and(with_db(pool))
+        .and_then(handlers::endpoint_remove)
+    }
 }
